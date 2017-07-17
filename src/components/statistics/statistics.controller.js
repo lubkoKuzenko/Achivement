ddcb744e2f85aecc8563deca502d35f746ce490c
Lyubomyr Kuzenko <l.kuzenko@levi9.com>
2017-07-17 15:33:12 +0300

    improve stats page

--- src/components/statistics/statistics.controller.js
+++ src/components/statistics/statistics.controller.js
@@ -5,9 +5,20 @@ export default class StatsController {
 	}
 
 	$onInit() {
+        this.yearStatistics();
+    }
+
+    yearStatistics(){
         this.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
         this.data = [
             [65, 59, 80, 81, 56, 55, 40]
+        ];
+    }
+
+    monthStatistics(){
+        this.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        this.data = [
+            [12, 1, 22, 66, 44, 32, 77, 44, 22, 15, 12, 88]
         ];
     }
 }
